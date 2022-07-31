6b596b46615fdb13b548308496c8d31ae564371f
xmayanx <mayasansu@gmail.com>
2022-07-30 20:46:13 -0700

    Rename foo to array1 and rename bar to array2

--- Program.cs
+++ Program.cs
@@ -4,15 +4,15 @@ class Program
 {
     public static void Main(string[] args)
     {
-        int[] foo = { 0, 1, 2, 3, 4 };
-        int[] bar = new int[100];
+        int[] array1 = { 0, 1, 2, 3, 4 };
+        int[] array2 = new int[100];
         for (int i = 0; i < 100; i++)
         {
-            bar[i] = i;
+            array2[i] = i;
         }
-        Console.WriteLine($"The total number of even numbers in foo are: {countEvenNum(foo, 5)}");
+        Console.WriteLine($"The total number of even numbers in array1 are: {countEvenNum(array1, 5)}");
 
-        Console.WriteLine($"The total number of even numbers in bar are: {countEvenNum(bar, 100)}");
+        Console.WriteLine($"The total number of even numbers in array2 are: {countEvenNum(array2, 100)}");
 
     }
 
